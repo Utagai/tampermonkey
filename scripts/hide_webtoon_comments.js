9193712d11211733ed062e3598c281470bdbe259
may h <mehrabhoque@gmail.com>
2020-07-24 02:46:35 -0400

    Allow ability to toggle comments on/off via the minus or plus keys

--- scripts/hide_webtoon_comments.js
+++ scripts/hide_webtoon_comments.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Hide the webtoons comment section
+// @name         Hide the cringey webtoons comment section
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
@@ -9,7 +9,19 @@
 // ==/UserScript==
 
 (function() {
-	'use strict';
+  'use strict';
 
-	document.getElementsByClassName("comment_area")[0].style.visibility = "hidden";
+  let comments = document.getElementsByClassName("comment_area")[0]
+  comments.style.visibility = "hidden";
+
+  document.onkeypress = function (e) {
+    e = e || window.event;
+    if (e.code === "Minus" || e.code == "Equal") {
+      if (comments.style.visibility === "hidden") {
+        comments.style.visibility = "";
+      } else {
+        comments.style.visibility = "hidden";
+      }
+    }
+  };
 })();
