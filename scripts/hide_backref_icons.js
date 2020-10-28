a2978d8789a6c0e52dd9018b614093268eca0f7f
may h <mehrabhoque@gmail.com>
2020-10-28 03:35:40 -0400

    Run backref hiding in a loop to account for document changes

--- scripts/hide_backref_icons.js
+++ scripts/hide_backref_icons.js
@@ -9,21 +9,23 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+	'use strict';
 
-    // OK, I mean, it's nasty but I wait for the page load here...
-    // Now the reason we can't just do something like window.addEventListener("load", ...)
-    // is because the backreferences are actually not in the DOM at the time the page is
-    // loaded. In fact, they are subtituted in after the page has finished loaded by some
-    // client-side javascript supplied by hackmd. A better snippet would probably execute
-    // a retry loop for something egregious like 10 seconds, but these scripts aren't
-    // really meant to be production quality, so to speak...
-    setTimeout(function() {
-        let footnote_backrefs = document.getElementsByClassName("footnote-backref");
-        for (let i = 0; i < footnote_backrefs.length; i++) {
-            footnote_backrefs[i].style.visibility = "hidden";
-        }
+	// OK, I mean, it's nasty but I wait for the page load here...
+	// Now the reason we can't just do something like window.addEventListener("load", ...)
+	// is because the backreferences are actually not in the DOM at the time the page is
+	// loaded. In fact, they are subtituted in after the page has finished loaded by some
+	// client-side javascript supplied by hackmd. A better snippet would probably execute
+	// a retry loop for something egregious like 10 seconds, but these scripts aren't
+	// really meant to be production quality, so to speak...
+	let hide = function() {
+		let footnote_backrefs = document.getElementsByClassName("footnote-backref");
+		for (let i = 0; i < footnote_backrefs.length; i++) {
+			footnote_backrefs[i].style.visibility = "hidden";
+		}
 
-        console.log("Hid " + footnote_backrefs.length + " backref icons.");
-    }, 3000)
+		console.log("Hid " + footnote_backrefs.length + " backref icons.");
+		setTimeout(hide, 1000);
+	}
+	setTimeout(hide, 1000)
 })();
