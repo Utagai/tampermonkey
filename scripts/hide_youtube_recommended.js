9a79dd2ac00922c68422678a1f35a6a786ba3ae3
May <mehrabhoque@gmail.com>
2020-12-29 02:22:57 -0500

    Some minor comment additions for my future self on hide_youtube_recommended.js

--- scripts/hide_youtube_recommended.js
+++ scripts/hide_youtube_recommended.js
@@ -11,16 +11,22 @@
 (function() {
     'use strict';
 
+    // This function will only have an impact on the initial page load, e.g., after a refresh, first visit, etc.
     let hide = function() {
         let related = document.getElementById("related");
         related.style.visibility = "hidden";
+        // Changing the ID here makes it so that this retry loop will fail to change anything once it successfully runs.
         related.id = "related-HIDING";
     };
+
+    // We have to run hide() in a loop because for some reason this part of the page doesn't load in all the time?
+    // Not sure, I don't care really either, it works. It's a dumb JS script.
     setInterval(hide, 3000);
 
     document.onkeypress = function (e) {
         e = e || window.event;
         if (e.code === "Minus" || e.code == "Equal") {
+            // Essentially, on press, swap the "related-SHOWING" to "related-HIDING" and vice-versa.
             let related = document.getElementById("related-SHOWING");
             if (related == null) {
                 let related = document.getElementById("related-HIDING");
