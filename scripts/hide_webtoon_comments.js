3532ba2c3b366426588921e053c1478c86256af6
May <mehrabhoque@gmail.com>
2020-12-29 01:18:37 -0500

    Update Tampermonkey script name

--- scripts/hide_webtoon_comments.js
+++ scripts/hide_webtoon_comments.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Hide the cringey webtoons comment section
+// @name         Hide the webtoons comment section
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
